ef5c9d2133b640283f0162f5b01366f102cc24ee
Ikshit Dhiman <75236509+dhiman-64@users.noreply.github.com>
2023-08-08 08:07:10 +0530

    Basics of Linked List

---
+++ 01_Basics.cpp
@@ -0,0 +1,19 @@
+/*
+    Linked List - Linear Data Structure made up from collection of Nodes
+                - Node -> consist of data and address of next Node
+    
+    
+    Why Linked List?
+    - Array -> Can't be changes it size on Runtime
+    - Vector -> Dynamic Data Structure Memory Wastage
+    - Linked List -> Best to use ( Grow/ Shrink on Runtime)
+                  -> No Memory Wastage
+                  -> Insertion / Deletion is Easy
+    
+    
+    Types of Linked List
+    - Singly Linked List
+    - Doubly Linked List
+    - Circular Linked List
+    
+*/
